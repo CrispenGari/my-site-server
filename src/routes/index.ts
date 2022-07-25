95606eb4f820eec0f131d06246d440bbc6c10a58
CrispenGari <Crispengari@gmail.com>
2022-07-25 13:59:33 +0200

    server:version2

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,61 @@
 import { Request, Response, Router } from "express";
-import { sendEmail , sendAutoReply} from "../utils";
+import { sendEmail, sendAutoReply } from "../utils";
+import Visitor from "../models";
 const router: Router = Router();
 
+router.get("/api/v1/visitors", async (_req: Request, res: Response) => {
+  try {
+    const visitors = await Visitor.find({});
+    return res.status(200).json({
+      visitors: visitors.length,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      code: 500,
+      message: "Internal Server error.",
+    });
+  }
+});
+
+router.post("/api/v1/visit", async (req: Request, res: Response) => {
+  try {
+    const { ip } = req.body;
+    const found = await Visitor.findOne({
+      ipAddress: ip,
+    });
+
+    if (found?.ipAddress) {
+      await Visitor.updateOne(
+        {
+          ipAddress: found.ipAddress,
+        },
+        {
+          $set: {
+            lastVisit: new Date(+new Date() + 7 * 24 * 60 * 60 * 1000),
+          },
+        }
+      );
+      return res.status(204).json({
+        status: 204,
+        message: "updated",
+      });
+    }
+    await Visitor.create({ ipAddress: ip as string });
+    return res.status(201).json({
+      status: 201,
+      message: "created",
+    });
+  } catch (e) {
+    return res.status(500).json({
+      code: 500,
+      message: "Internal Server error.",
+    });
+  }
+});
+
 router.get("/", (_req: Request, res: Response) => {
-  res.status(200).json({
+  console.log(_req.session);
+  return res.status(200).json({
     name: "backend",
     language: "typescript",
     message: "hello world!",
@@ -32,7 +84,7 @@ router.post("/api/v1/send-email", async (req: Request, res: Response) => {
     await sendEmail(html, subject, from, fullName);
     // automatic reply
 
-    const autoReplyHTML:string = `
+    const autoReplyHTML: string = `
     <h2>${subject}</h2>
 
     <p>This email is an automatic response for your request to us on your email of the subject <b>"${subject}"</b>. We will respond to your email as soon as possible.</p><p><b>Please do not reply to this email</b></p>
@@ -40,15 +92,14 @@ router.post("/api/v1/send-email", async (req: Request, res: Response) => {
     <h5>Kind Regards</h5>
 <br/>
     Crispen
-    `
-    await sendAutoReply(autoReplyHTML, subject, from)
+    `;
+    await sendAutoReply(autoReplyHTML, subject, from);
 
     return res.status(200).json({
       code: 200,
       message: "The email was sent successfully.",
     });
   } catch (e) {
-    console.log(e.message);
     return res.status(500).json({
       code: 500,
       message: "Failed to send the email.",
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,48 @@
 import "dotenv/config";
+import loadEvTypes from "node-env-types";
 import express from "express";
 import cors from "cors";
 import router from "./routes";
+import session from "express-session";
+import { __cookieName__, __maxAge__ } from "./constants";
+
+loadEvTypes();
 
 // ----
-const app: express.Application = express();
+
 const PORT: any = process.env.PORT || 3001;
 const HOST: string = "0.0.0.0";
-//
-app.use(
-  cors({
-    origin: "https://crispengari-ac2c8.web.app",
+
+(async () => {
+  const app: express.Application = express();
+  await require("./connection");
+  app.use(
+    cors({
+      origin: "https://crispengari-ac2c8.web.app",
+    })
+  );
+  app.use(express.json());
+  app.use(
+    session({
+      saveUninitialized: false,
+      secret: process.env.SESSION_SECRETE,
+      resave: false,
+      name: __cookieName__,
+      cookie: {
+        maxAge: __maxAge__,
+        httpOnly: true,
+        secure: false, // https when true
+        sameSite: "lax",
+      },
+    })
+  );
+  app.use(router);
+  app.listen(PORT, HOST);
+})()
+  .then(() => {
+    console.log(`The server is running on port: ${PORT}`);
   })
-);
-app.use(express.json());
-app.use(router);
-app.listen(PORT, HOST, () => {
-  console.log(`The server is running on port: ${PORT}`);
-});
+  .catch((err) => {
+    console.log(err);
+  });
+//
